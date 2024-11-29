4fc0b7be854bde06e6c7a472480a4247d32564ba
Jean <63162857+masckmaster2007@users.noreply.github.com>
2024-11-29 21:06:11 +0100

    import check

--- incl/lib/connection.php
+++ incl/lib/connection.php
@@ -13,22 +13,23 @@ if(empty($db)) {
 	try {
 		$db = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password, array(PDO::ATTR_PERSISTENT => true));
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		// database.sql existance check!
+		try {
+			$result = $db->query("SELECT 1 FROM levels LIMIT 1");
+		} catch (Exception $e) {
+			$error = true;
+		}
+		
+		if ($error || $result === FALSE) {
+			$db->exec(file_get_contents(__DIR__ . "/../../database.sql"));
+		}
+		
 		$ic->checkIP();
 		$ip = $ic->getYourIP();
 		if($minGameVersion != 0 && isset($_POST['gameVersion']) && $_POST['gameVersion'] != 0 && $_POST['gameVersion'] < $minGameVersion && !isset($_SESSION)) exit("-1");
 		if($maxGameVersion != 0 && isset($_POST['gameVersion']) && $_POST['gameVersion'] != 0 && $_POST['gameVersion'] > $maxGameVersion && !isset($_SESSION)) exit("-1");
 		if($minBinaryVersion != 0 && isset($_POST['binaryVersion']) && $_POST['binaryVersion'] != 0 && $_POST['binaryVersion'] < $minBinaryVersion && !isset($_SESSION)) exit("-1");
 		if($maxBinaryVersion != 0 && isset($_POST['binaryVersion']) && $_POST['binaryVersion'] != 0 && $_POST['binaryVersion'] > $maxBinaryVersion && !isset($_SESSION)) exit("-1");
-
-		    try {
-			$result = $db->query("SELECT 1 FROM levels LIMIT 1");
-		    } catch (Exception $e) {
-			$error = true;
-		    }
-		
-		    if ($error || $result === FALSE) {
-			$db->exec(file_get_contents(__DIR__ . "/../../database.sql"));
-		    }
 		
 		if(!isset($_SESSION['accountID'])) {
 			$getExtID = $db->prepare('SELECT extID FROM users WHERE isRegistered = 1 AND IP = :ip LIMIT 1');
