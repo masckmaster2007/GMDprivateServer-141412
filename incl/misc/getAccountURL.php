45a5e0064ddfbdeaf7a093424a56c3f912881219
Jean <63162857+masckmaster2007@users.noreply.github.com>
2025-08-08 07:25:53 +0300

    fix?

--- incl/misc/getAccountURL.php
+++ incl/misc/getAccountURL.php
@@ -1,5 +1,5 @@
 <?php
-if((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443) $https = 'https';
+if((!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')) || (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443) $https = 'https';
 else $https = 'http';
 echo dirname($https."://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
 ?>
